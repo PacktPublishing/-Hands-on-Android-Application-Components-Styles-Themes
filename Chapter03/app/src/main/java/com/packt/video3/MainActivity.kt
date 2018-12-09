1474f8dd2a18e6c407af99115f25f902a7be9b65
Ehsan <Ehsan@MacBook-Pro.local>
2018-12-09 20:15:35 +0330

    Adding Chapter03 and Chapter04

---
+++ Chapter03/app/src/main/java/com/packt/video3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.packt.video3
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
