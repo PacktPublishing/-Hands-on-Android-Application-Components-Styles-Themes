2d4967eb6ee1c7f27242aecc9f3eec051dedbaf3
Ehsan <Ehsan@MacBook-Pro.local>
2018-12-28 02:23:19 +0330

    Add ResponsiveDesign-1.2 project

---
+++ Section01/ResponsiveDesign-1.2/app/src/main/java/com/packt/responsivedesign/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.packt.responsivedesign
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
